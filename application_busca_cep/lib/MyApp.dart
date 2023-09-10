464ac1773b44d6ee3d15261d97d761c42c3fbdd7
Kawan02 <kawanng39@gmail.com>
2023-09-10 09:13:48 -0300

    check internet

--- application_busca_cep/lib/MyApp.dart
+++ application_busca_cep/lib/MyApp.dart
@@ -1,6 +1,7 @@
 import 'package:application_busca_cep/buscar_cep.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:get/get.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -12,7 +13,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
