aa05d7daf500edd9bfa832ac26a92dc9ae755af4
Kawan Messias <kawan.messias@trescon.com.br>
2023-09-21 14:44:40 -0300

    Implementação do tema dark de acordo com o sistema

--- application_busca_cep/lib/MyApp.dart
+++ application_busca_cep/lib/MyApp.dart
@@ -1,4 +1,5 @@
 import 'package:application_busca_cep/buscar_cep.dart';
+import 'package:application_busca_cep/widgets/tema.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
@@ -10,19 +11,47 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    PreferenciaTema.setTema();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    PreferenciaTema.setTema();
+    super.didChangePlatformBrightness();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      locale: Locale('pt', 'BR'),
-      supportedLocales: [Locale('pt', 'BR')],
-      home: BuscarCep(),
+    return ValueListenableBuilder(
+      valueListenable: PreferenciaTema.tema,
+      builder: (BuildContext context, Brightness tema, _) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          locale: const Locale('pt', 'BR'),
+          supportedLocales: const [Locale('pt', 'BR')],
+          theme: ThemeData(
+            primarySwatch: Colors.deepOrange,
+            brightness: tema,
+          ),
+          home: const BuscarCep(),
+        );
+      },
     );
   }
 }
