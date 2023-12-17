e354bbbb38975925d0d8f5c69bce3ef65bce926b
Kawan02 <kawanng39@gmail.com>
2023-12-17 11:56:27 -0300

    Ajustes e melhorias

--- application_busca_cep/lib/src/respostas/respostas.dart
+++ application_busca_cep/lib/src/respostas/respostas.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:application_busca_cep/src/model/cep_model.dart';
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:motion_toast/motion_toast.dart';
 
 Future<AwesomeDialog> showErrorDialog(String? message, BuildContext context) async {
   return AwesomeDialog(
@@ -45,4 +46,21 @@ FutureOr<AwesomeDialog> showWarningDialog(BuildContext context, CepModel cepMode
     btnCancelOnPress: () {},
     btnOkOnPress: () {},
   )..show();
+}
+
+void showToast({required String message, bool isError = false, required context}) {
+  MotionToast(
+    icon: isError ? Icons.info : Icons.check_circle,
+    primaryColor: isError ? Colors.red : Colors.green,
+    description: Text(
+      message,
+      style: const TextStyle(
+        fontSize: 14,
+        color: Colors.black,
+      ),
+    ),
+    width: 200,
+    height: 100,
+    padding: const EdgeInsets.symmetric(vertical: 40),
+  ).show(context);
 }
