354c50b431a761030f07435f599faa671e253c3f
Kawan Messias <kawan.messias@trescon.com.br>
2023-12-19 09:48:35 -0300

    Alterando o gerenciamento de estado com o GetX

--- application_busca_cep/lib/src/controller/buscar_cep_controller.dart
+++ application_busca_cep/lib/src/controller/buscar_cep_controller.dart
@@ -7,8 +7,9 @@ import 'package:application_busca_cep/src/respostas/respostas.dart';
 import 'package:application_busca_cep/src/services/api_service.dart';
 import 'package:application_busca_cep/src/widgets/cep_modal.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class BuscarCepController extends ChangeNotifier {
+class BuscarCepController extends GetxController {
   final ApiService apiService = ApiService();
   final ValueNotifier<CepModel?> adress = ValueNotifier(null);
   final ValueNotifier<bool> isLoading = ValueNotifier(false);
@@ -35,45 +36,47 @@ class BuscarCepController extends ChangeNotifier {
       adress.value = model;
       showAlertDialog(context, model, buscarCepController!, controller!);
       isLoading.value = false;
-      notifyListeners();
+      update();
     } else {
       adress.value = null;
       isLoading.value = false;
-      notifyListeners();
+      update();
     }
   }
 
   // Salva os dados
-  FutureOr<void> salvarTarefa(CepModel cepModel) async {
+  FutureOr<void> salvarTarefa(CepModel cepModel, BuildContext context) async {
     await ObjectBoxDatabase.tarefaBox.putAsync(cepModel);
     todos.add(cepModel);
-    notifyListeners();
+    Navigator.of(context).pop();
+    update();
   }
 
   // Exclui os dados
-  FutureOr<void> deletarTarefa(CepModel cepModel) async {
+  FutureOr<void> deletarTarefa(CepModel cepModel, BuildContext context) async {
     await ObjectBoxDatabase.tarefaBox.removeAsync(cepModel.id);
-    notifyListeners();
+    update();
+    showToast(
+      message: "CEP: ${cepModel.cepController} excluído com sucesso!",
+      context: context,
+    );
   }
 
   // Filtra todos os dados
   List<CepModel> encontrarTarefa() {
     _todos = ObjectBoxDatabase.tarefaBox.query().order(CepModel_.dataTime, flags: Order.descending).build().find();
-    notifyListeners();
     return _todos;
   }
 
   //Filtra só pelo CEP, digitando os numeros do CEP inicial ele já busca
   List<CepModel> listFilter(TextEditingController cepFilter) {
     var filter = ObjectBoxDatabase.tarefaBox.query(CepModel_.cepController.startsWith(cepFilter.text)).build().find();
-    notifyListeners();
     return filter;
   }
 
 //Filtra só pelo CEP se estiver igual
   List<CepModel> encontrarTodasTarefa(TextEditingController controllerTextField) {
     var filterCep = ObjectBoxDatabase.tarefaBox.query(CepModel_.cepController.equals(controllerTextField.text)).build().find();
-    notifyListeners();
     return filterCep;
   }
 }
--- application_busca_cep/lib/src/pages/cep/filter/cep_filter.dart
+++ application_busca_cep/lib/src/pages/cep/filter/cep_filter.dart
@@ -1,81 +1,79 @@
 import 'package:application_busca_cep/src/controller/buscar_cep_controller.dart';
 import 'package:application_busca_cep/src/pages/cep/components/google_maps.dart';
-import 'package:application_busca_cep/src/respostas/respostas.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class CepFilter extends StatefulWidget {
+class CepFilter extends StatelessWidget {
   final BuscarCepController? buscarCepController;
   final TextEditingController controllerFilter;
-  const CepFilter({super.key, this.buscarCepController, required this.controllerFilter});
+  const CepFilter({
+    super.key,
+    this.buscarCepController,
+    required this.controllerFilter,
+  });
 
-  @override
-  State<CepFilter> createState() => _CepFilterState();
-}
-
-class _CepFilterState extends State<CepFilter> {
   @override
   Widget build(BuildContext context) {
     return Visibility(
-      visible: !widget.buscarCepController!.isLoading.value,
+      visible: !buscarCepController!.isLoading.value,
       replacement: const Center(child: CircularProgressIndicator()),
-      child: ListView.builder(
-        physics: const BouncingScrollPhysics(),
-        padding: const EdgeInsets.only(top: 10),
-        shrinkWrap: true,
-        itemCount: widget.buscarCepController!.listFilter(widget.controllerFilter).length,
-        itemBuilder: (context, index) {
-          final cepFilter = widget.buscarCepController!.listFilter(widget.controllerFilter)[index];
-          return Dismissible(
-            background: Container(
-              color: Colors.red,
-              child: const Align(
-                alignment: Alignment(-0.9, 0),
-                child: Icon(
-                  Icons.delete,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            direction: DismissDirection.startToEnd,
-            key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
-            child: ListTile(
-              title: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "CEP: ${cepFilter.cepController!.isEmpty ? "CEP vazio ou não encontrado" : cepFilter.cepController!}",
-                  ),
-                  Text("Criado em: ${cepFilter.dataTime}")
-                ],
-              ),
-              leading: CircleAvatar(
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    image: const DecorationImage(
-                      image: AssetImage("assets/imgs/correios.png"),
+      child: GetBuilder(
+        init: buscarCepController,
+        builder: (BuscarCepController controller) {
+          return ListView.builder(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.only(top: 10),
+            shrinkWrap: true,
+            itemCount: controller.listFilter(controllerFilter).length,
+            itemBuilder: (context, index) {
+              final cepFilter = controller.listFilter(controllerFilter)[index];
+              return Dismissible(
+                background: Container(
+                  color: Colors.red,
+                  child: const Align(
+                    alignment: Alignment(-0.9, 0),
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
                     ),
                   ),
                 ),
-              ),
-              subtitle: Text(
-                "${cepFilter.logradouro}, ${cepFilter.bairro}, ${cepFilter.cidade}, ${cepFilter.uf}. ${cepFilter.ddd}",
-              ),
-              trailing: IconButton(
-                onPressed: () async => await abrirGoogleMaps(cepFilter.cepController!, context),
-                icon: const Icon(
-                  Icons.location_on,
-                  color: Colors.greenAccent,
-                  size: 30,
+                direction: DismissDirection.startToEnd,
+                key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+                child: ListTile(
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "CEP: ${cepFilter.cepController!.isEmpty ? "CEP vazio ou não encontrado" : cepFilter.cepController!}",
+                      ),
+                      Text("Criado em: ${cepFilter.dataTime}")
+                    ],
+                  ),
+                  leading: CircleAvatar(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        image: const DecorationImage(
+                          image: AssetImage("assets/imgs/correios.png"),
+                        ),
+                      ),
+                    ),
+                  ),
+                  subtitle: Text(
+                    "${cepFilter.logradouro}, ${cepFilter.bairro}, ${cepFilter.cidade}, ${cepFilter.uf}. ${cepFilter.ddd}",
+                  ),
+                  trailing: IconButton(
+                    onPressed: () async => await abrirGoogleMaps(cepFilter.cepController!, context),
+                    icon: const Icon(
+                      Icons.location_on,
+                      color: Colors.greenAccent,
+                      size: 30,
+                    ),
+                  ),
+                  dense: true,
                 ),
-              ),
-              dense: true,
-            ),
-            onDismissed: (direction) async {
-              await widget.buscarCepController!.deletarTarefa(cepFilter);
-              showToast(
-                message: "CEP: ${cepFilter.cepController} excluido com sucesso!",
-                context: context,
+                onDismissed: (direction) async => await controller.deletarTarefa(cepFilter, context),
               );
             },
           );
